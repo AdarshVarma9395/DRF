27fa18be01c27685ff3c5a293d08f14729d1ad64
AdarshVarma9395 <adarshvarma1999@gmail.com>
2024-09-04 07:45:40 +0530

    Rest-api

---
+++ myapp/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from .serializers import TaskSerializer
+from .models import Task
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view(['GET'])
+def apiOverview(request):
+    api_urls = {
+        'List': '/task-list/',
+        'Detail View': '/task-detail/<str:pk>/',
+        'Create' : '/task-create/',
+        'Update' : '/task-update/<str:pk>/',
+        'delete' : '/task-delete/<str:pk>/',
+    }
+    return Response(api_urls)
+
+@api_view(['GET'])
+def taskList(request):
+    tasks = Task.objects.all()
+    serializer = TaskSerializer(tasks, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def taskDetail(request,pk):
+    tasks = Task.objects.get(id=pk)
+    serializer = TaskSerializer(tasks, many=False)
+    return Response(serializer.data)
